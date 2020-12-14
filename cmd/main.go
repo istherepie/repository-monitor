19c8baecd592f447e802b4743e6d646d18ee129e
Steffen Park <dev@istherepie.com>
2020-12-14 16:12:08 +0100

    Add startup procedure to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/istherepie/request-monitor/webserver"
 )
 
-func run() int {
-	listener, err := net.Listen("tcp", "localhost:8000")
+func run(ID string, hostname string, port int, logger *log.Logger) int {
+	// Setup listener
+	address := fmt.Sprintf("%v:%d", hostname, port)
+	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
-		log.Println(err)
+		logger.Println(err)
 		return 1
 	}
 
 	defer listener.Close()
 
-	logger := log.New(os.Stdout, "Access ", log.Ldate|log.Ltime)
+	// Init multiplexer
+	mux := webserver.Mux(ID, logger)
 
-	mux := webserver.Mux("service-1", logger)
-
+	// Serve
 	serveError := http.Serve(listener, mux)
 
 	if serveError != nil {
-		log.Println(serveError)
+		logger.Println(serveError)
 		return 1
 	}
 
@@ -34,6 +39,38 @@ func run() int {
 }
 
 func main() {
-	code := run()
+
+	// Service port
+	var port int
+
+	// Get Args
+	serviceID := flag.String("id", "service-1", "Meta ID/name of the service")
+	servicePort := flag.Int("port", 8080, "Service Port (tcp)")
+	serviceHost := flag.String("host", "localhost", "Service hostname/IP address")
+	flag.Parse()
+
+	if !flag.Parsed() {
+		flag.Usage()
+		return
+	}
+
+	// GET ENV VARS
+	envServicePort := os.Getenv("RM_SERVICE_PORT")
+
+	// Setup logger
+	loggerPrefix := fmt.Sprintf("[%v] ", *serviceID)
+	logger := log.New(os.Stdout, loggerPrefix, log.Ldate|log.Ltime)
+
+	altPort, err := strconv.ParseInt(envServicePort, 10, 64)
+
+	// Port set by env variable overrides flag
+	if err == nil {
+		port = int(altPort)
+	} else {
+		port = *servicePort
+	}
+
+	// Start
+	code := run(*serviceID, *serviceHost, port, logger)
 	os.Exit(code)
 }
